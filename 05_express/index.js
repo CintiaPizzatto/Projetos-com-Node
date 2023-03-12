b3f733cc5a492c11fba040c5c7b9722f767825a3
Cintia Pizzatto <cintiapizzatto@hotmail.com>
2023-03-12 19:15:41 -0300

    update app.js

--- 05_express/index.js
+++ 05_express/index.js
@@ -7,13 +7,16 @@ let app = express(); // This is the same as the above code
 // o método get() é um método do objeto app
 // o método get() recebe dois parâmetros: a rota e uma função
 // a função recebe dois parâmetros: request e response
-// o método send() é um método do objeto response
-// o método send() envia uma resposta para o navegador
-// o método send() recebe um parâmetro: uma string
-// o método send() envia uma string para o navegador
+// o método send() é um método do objeto response, que envia uma resposta para o navegador
+// o método send() recebe um parâmetro: uma string e envia essa string para o navegador
+// o método listen() é um método do objeto app, e recebe dois parâmetros: a porta e uma função
 // vamos mandar uma resposta para o nosso usuário. Não precisamos nos preocupar com o a solicitação, pois aqui o usuário não está fazendo uma solicitação, ele está acessando a página inicial
 app.get('/', (request, response) => { // cria uma rota para a página inicial    
     response.send('<h1>Primeira rota com Express!</h1>'); // envia uma resposta para o navegador    
+});
+
+app.get('/teste', (request, response) => { // cria uma rota para a página inicial    
+    response.send('Segunda rota com Express!'); // envia uma resposta para o navegador    
 });
 
 app.listen(3000, () => { // cria um servidor na porta 3000      
